1c2976643e533a4077ce7d585f8dcea9a7cec753
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2026-06-22 20:39:22 +0530

    Update Community.jsx

--- client/src/pages/Community.jsx
+++ client/src/pages/Community.jsx
@@ -4,7 +4,7 @@ import { Heart } from 'lucide-react';
 import axios from 'axios';
 import toast from 'react-hot-toast';
 
-axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL  || "https://ai-saas-app-backend-7zwi.onrender.com";
 
 const Community = () => {
 
